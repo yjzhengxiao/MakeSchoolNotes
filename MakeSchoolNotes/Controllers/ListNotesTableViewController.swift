c4a8a1fffff1ce0ad394f09f9b016e000d213613
Xiao Zheng <yjzhengxiao@126.com>
2016-06-22 15:34:38 -0700

    adding // 1

--- MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
+++ MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
@@ -13,5 +13,22 @@ class ListNotesTableViewController: UITableViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
   }
+    
+    // 1
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    // 2
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        // 3
+        let cell = tableView.dequeueReusableCellWithIdentifier("listNotesTableViewCell", forIndexPath: indexPath)
+        
+        // 4
+        cell.textLabel?.text = "Yay - it's working!"
+        
+        // 5
+        return cell
+    }
   
 }
