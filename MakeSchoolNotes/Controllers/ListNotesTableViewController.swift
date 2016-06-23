94a3ff8f3e631e1cc1f1b4b92ed48f2857152b6a
Xiao Zheng <yjzhengxiao@126.com>
2016-06-23 09:25:35 -0700

    Typecasting our Cell to ListNotesTableViewCell

--- MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
+++ MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
@@ -9,10 +9,6 @@
 import UIKit
 
 class ListNotesTableViewController: UITableViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
     
     // 1
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -21,14 +17,20 @@ class ListNotesTableViewController: UITableViewController {
     
     // 2
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
         // 3
-        let cell = tableView.dequeueReusableCellWithIdentifier("listNotesTableViewCell", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("listNotesTableViewCell", forIndexPath: indexPath) as! ListNotesTableViewCell
         
         // 4
-        cell.textLabel?.text = "Yay - it's working!"
+        cell.noteTitleLabel.text = "note's title"
+        cell.noteModificationTimeLabel.text = "note's modification time"
         
-        // 5
         return cell
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
   
 }
--- MakeSchoolNotes/Views/ListNotesTableViewCell.swift
+++ MakeSchoolNotes/Views/ListNotesTableViewCell.swift
@@ -10,5 +10,9 @@ import UIKit
 
 class ListNotesTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var noteTitleLabel: UILabel!
+    
+    @IBOutlet weak var noteModificationTimeLabel: UILabel!
+    
 
 }
