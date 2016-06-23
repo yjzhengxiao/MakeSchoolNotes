64007bd658528fdc368de37ad486e8533ff44880
Xiao Zheng <yjzhengxiao@126.com>
2016-06-23 11:09:03 -0700

    Introduction to Segues

--- MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
+++ MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
@@ -32,5 +32,16 @@ class ListNotesTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // 1
+        if let identifier = segue.identifier {
+            // 2
+            if identifier == "displayNote" {
+                // 3
+                print("Transitioning to the Display Note View Controller")
+            }
+        }
+    }
   
 }
